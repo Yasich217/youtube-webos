fdd4662e8670e5304031d99467201b25766bb89f
Yaroslav Kazarin <Kazarin-Yaroslav@yandex.ru>
2023-06-20 20:55:42 +0300

    fix visible segments when slider has markers

--- src/sponsorblock.js
+++ src/sponsorblock.js
@@ -153,9 +153,8 @@ class SponsorBlockHandler {
         color: 'blue',
         opacity: 0.7
       };
-      const transform = `translateX(${
-        (start / videoDuration) * 100.0
-      }%) scaleX(${(end - start) / videoDuration})`;
+      const transform = `translateX(${(start / videoDuration) * 100.0
+        }%) scaleX(${(end - start) / videoDuration})`;
       const elm = document.createElement('div');
       elm.classList.add('ytlr-progress-bar__played');
       elm.style['background'] = barType.color;
@@ -179,13 +178,20 @@ class SponsorBlockHandler {
     });
 
     this.sliderInterval = setInterval(() => {
-      this.slider = document.querySelector('.ytlr-progress-bar__slider');
+      const sliderWithMarkers = document.querySelector('.ytlr-progress-bar__slider-base');
+      const sliderWithoutMarkers = document.querySelector('.ytlr-progress-bar__slider');
+
+      this.slider = sliderWithoutMarkers ?? sliderWithMarkers;
       if (this.slider) {
         clearInterval(this.sliderInterval);
         this.sliderInterval = null;
         this.observer.observe(this.slider, {
           childList: true
         });
+
+        if (sliderWithMarkers) {
+          this.segmentsoverlay.style.marginTop = '-1.19em';
+        }
         this.slider.appendChild(this.segmentsoverlay);
       }
     }, 500);
